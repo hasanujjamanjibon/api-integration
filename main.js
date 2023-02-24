15e607e69e272ec89b19ee2de9931a4bcadcf42c
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2023-02-25 02:37:41 +0600

    update

--- main.js
+++ main.js
@@ -1,57 +1,60 @@
-const loadFishData = (seartchtext) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${seartchtext}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => fishDataDetails(data.meals.slice(0, 8)));
+const modalTitile = document.getElementById("staticBackdropLabel");
+const modalImages = document.getElementById("card-images");
+const fishContainer = document.getElementById("fishContainer");
+
+let limit = 8;
+let searchTerm = "Chicken";
+const baseApiUrl = "https://www.themealdb.com/api/json/v1/1/";
+
+const loadMealData = async (isLoadNext) => {
+  const url = `${baseApiUrl}search.php?s=${searchTerm}`;
+
+  const res = await fetch(url);
+  let { meals } = await res.json();
+
+  if (isLoadNext) meals = meals.slice(limit, meals.length);
+  else meals = meals.slice(0, limit);
+
+  fishDataDetails(meals);
 };
 
-const ModalTitile = document.getElementById("staticBackdropLabel");
-const modalImages = document.getElementById("card-images");
-const showAllData = (seartchtext) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${seartchtext}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => fishDataDetails(data.meals));
-};
 function fishDataDetails(data) {
-  const fishContainer = document.getElementById("fishContainer");
   fishContainer.innerHTML = "";
-  data.forEach((MealData) => {
-    const fishElement = document.createElement("div");
-    fishElement.classList.add("fishElelement");
-    fishElement.classList.add("shadow-lg");
-    fishElement.innerHTML = ` 
+  data.forEach((mealData) => {
+    fishContainer.innerHTML += ` 
+      <div class="fishElelement shadow-lg">
         <img class="w-full h-60 rounded-md" src="${
-          MealData.strMealThumb ? MealData.strMealThumb : "No Picture"
+          mealData.strMealThumb ? mealData.strMealThumb : "No Picture"
         }" alt="">
-        <h4 class="text-3xl font-semibold py-6">${MealData.strMeal}</h4>
-        <button onclick="showData(${MealData.idMeal})"  data-bs-toggle="modal"
+        <h4 class="text-3xl font-semibold py-6">${mealData.strMeal}</h4>
+        <button onclick="showData(${mealData.idMeal})"  data-bs-toggle="modal"
         data-bs-target="#staticBackdrop" class="px-4 py-3 bg-slate-600 rounded-md text-white">Show Details</button>
+      </div>
     `;
-    fishContainer.appendChild(fishElement);
   });
 }
 
-function showData(MealData) {
-  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${MealData}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => loadModalData(data.meals[0]));
+async function showData(mealData) {
+  const url = `${baseApiUrl}lookup.php?i=${mealData}`;
+  const res = await fetch(url);
+  const { meals } = await res.json();
+  loadModalData(meals[0]);
 }
 
 function loadModalData(data) {
-  ModalTitile.innerHTML = data.strMeal;
+  modalTitile.innerHTML = data.strMeal;
   const modalBody = document.getElementById("Modal-body");
   modalBody.innerHTML = `
   <img class="w-full h-72" src='${data.strMealThumb}' alt="" />
  `;
   console.log(data.strMealThumb);
 }
+
 document.getElementById("basic-addon2").addEventListener("click", function () {
   const inputElement = document.getElementById("input-field");
-  const ValueOfinputElement = inputElement.value;
+  searchTerm = inputElement.value;
   inputElement.value = "";
-  loadFishData(ValueOfinputElement);
+  loadMealData();
 });
 
-loadFishData("Chicken");
+loadMealData();
